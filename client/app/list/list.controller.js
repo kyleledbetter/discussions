4842dd29cd58c4dc900effb1b71e1463b6066c50
kyleledbetter <kyle@kyleledbetter.com>
2016-04-18 00:03:18 -0500

    update(list): link to new form

--- client/app/list/list.controller.js
+++ client/app/list/list.controller.js
@@ -25,34 +25,6 @@ angular.module('appApp')
       );
       $scope.joined = !$scope.joined;
     };
-    $scope.getColor = function($index) {
-      var _d = ($index + 1) % 11;
-      var bg = '';
-
-      switch(_d) {
-        case 1: bg = 'red'; break;
-        case 2: bg = 'green'; break;
-        case 3: bg = 'darkBlue'; break;
-        case 4: bg = 'blue'; break;
-        case 5: bg = 'yellow'; break;
-        case 6: bg = 'pink'; break;
-        case 7: bg = 'darkBlue'; break;
-        case 8: bg = 'purple'; break;
-        case 9: bg = 'deepBlue'; break;
-        case 10: bg = 'lightPurple'; break;
-        default: bg = 'yellow'; break;
-      }
-
-      return bg;
-    };
-
-    $scope.getSpan = function($index) {
-      var _d = ($index + 1) % 11;
-
-      if (_d === 1 || _d === 5) {
-        return 2;
-      }
-    };
 
     // begin dialog
     $scope.status = '  ';
