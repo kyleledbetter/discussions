4cdbfba2d20f16da9757c74ff3410c82aab7fb02
KL186023 <Kyle.Ledbetter@teradata.com>
2016-04-18 19:17:20 -0500

    update(bookmarks): bookmark status, link to detail

--- client/app/bookmarks/bookmarks.controller.js
+++ client/app/bookmarks/bookmarks.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('appApp')
-  .controller('BookmarksCtrl', function ($scope, $state, $http, $mdToast) {
+  .controller('BookmarksCtrl', function ($scope, $http, $state, $mdDialog, $mdMedia, $mdToast) {
     $scope.awesomeThings = [];
     $scope.$state = $state;
     $http.get('/api/things').success(function(awesomeThings) {
@@ -16,4 +16,73 @@ angular.module('appApp')
           .hideDelay(2000)
       );
     };
+    // begin dialog
+    $scope.status = '  ';
+    $scope.customFullscreen = $mdMedia('xs') || $mdMedia('sm');
+    $scope.showAlert = function(ev) {
+      // Appending dialog to document.body to cover sidenav in docs app
+      // Modal dialogs should fully cover application
+      // to prevent interaction outside of dialog
+      $mdDialog.show(
+        $mdDialog.alert()
+          .parent(angular.element(document.querySelector('#popupContainer')))
+          .clickOutsideToClose(true)
+          .title('This is an alert title')
+          .textContent('You can specify some description text in here.')
+          .ariaLabel('Alert Dialog Demo')
+          .ok('Got it!')
+          .targetEvent(ev)
+      );
+    };
+    $scope.showDetail = function(ev) {
+      // var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
+      $mdDialog.show({
+        controller: DialogController,
+        templateUrl: 'app/list/dialog/dialog.html',
+        parent: angular.element(document.querySelector('#main')),
+        // parent: angular.element(document.body),
+        targetEvent: ev,
+        clickOutsideToClose: true
+      })
+      .then(function(answer) {
+        $scope.status = 'You said the information was "' + answer + '".';
+      }, function() {
+        $scope.status = 'You cancelled the dialog.';
+      });
+      $scope.$watch(function() {
+        return $mdMedia('xs') || $mdMedia('sm');
+      }, function(wantsFullScreen) {
+        $scope.customFullscreen = (wantsFullScreen === true);
+      });
+    };
+    // end dialog
   });
+  function DialogController($scope, $mdDialog, $mdToast, $state, $timeout, $mdBottomSheet) {
+    $scope.hide = function() {
+      $mdDialog.hide();
+    };
+    $scope.cancel = function() {
+      $mdDialog.cancel();
+    };
+    $scope.answer = function(answer) {
+      $mdDialog.hide(answer);
+    };
+    $scope.bookmark = function(){
+      $scope.bookmarked = !$scope.bookmarked;
+      $mdToast.show(
+        $mdToast.simple()
+          .textContent('Bookmarked!')
+          .position('top right')
+          .hideDelay(2000)
+      );
+    };
+    $scope.alert = '';
+    $scope.replyForm = function() {
+      $scope.alert = '';
+      $mdBottomSheet.show({
+        templateUrl: 'app/detail/reply/form/form.html',
+        controller: 'FormCtrl',
+        parent: angular.element(document.querySelector('#main'))
+      });
+    };
+  }
--- server/api/thing/thing.controller.js
+++ server/api/thing/thing.controller.js
@@ -17,26 +17,32 @@ exports.index = function(req, res) {
   {
   name : 'Development Tools',
   icon: 'camera_rear',
+  bookmarked: true,
   info : 'Integration with popular tools such as Bower, Grunt, Karma, Mocha, JSHint, Node Inspector, Livereload, Protractor, Jade, Stylus, Sass, CoffeeScript, and Less.'
   }, {
   name : 'Server and Client integration',
   icon: 'business',
+  bookmarked: true,
   info : 'Built with a powerful and fun stack: MongoDB, Express, AngularJS, and Node.'
   }, {
   name : 'Smart Build System',
   icon: 'cast_connected',
+  bookmarked: true,
   info : 'Build system ignores `spec` files, allowing you to keep tests alongside code. Automatic injection of scripts and styles into your index.html'
   },  {
   name : 'Modular Structure',
   icon: 'dashboard',
+  bookmarked: true,
   info : 'Best practice client and server structures allow for more code reusability and maximum scalability'
   },  {
   name : 'Optimized Build',
   icon: 'cloud_done',
+  bookmarked: false,
   info : 'Build process packs up your templates as a single JavaScript payload, minifies your scripts/css/images, and rewrites asset names for caching.'
   },{
   name : 'Deployment Ready',
   icon: 'account_balance_wallet',
+  bookmarked: false,
   info : 'Easily deploy your app to Heroku or Openshift with the heroku and openshift subgenerators'
   }
   ]);
